0d04343afb98c9da0acc1dec1809409054287350
nic0michael <nicomichael2012@gmail.com>
2020-12-06 18:49:38 +0200

    changed the package name

--- src/main/java/za/co/kanban/repositories/AntennaGainDataRepository.java
+++
@@ -1,9 +0,0 @@
-package za.co.kanban.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import za.co.kanban.model.AntennaGainData;
-
-public interface AntennaGainDataRepository extends JpaRepository<AntennaGainData, Long> {
-	
-}
